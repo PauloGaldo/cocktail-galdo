898e328b9fb43bba5139ce6f0db684bac78bcad3
Paulo Galdo <galdopaulo@gmail.com>
2025-10-27 00:46:39 -0300

    fix: Update COCKTAIL_API to use HTTPS for secure requests

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const COCKTAIL_KEY = '1';
-export const COCKTAIL_API = `http://www.thecocktaildb.com/api/json/v1/${COCKTAIL_KEY}/`;
+export const COCKTAIL_API = `https://www.thecocktaildb.com/api/json/v1/${COCKTAIL_KEY}/`;
 export const COCKTAIL_IMAGE_API = `https://www.thecocktaildb.com/images/ingredients/`;
