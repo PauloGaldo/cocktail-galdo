f77e74cf485c3334c71dcdd09c88f0b35f754399
Paulo Galdo <galdopaulo@gmail.com>
2025-10-27 16:24:37 -0300

    refactor: integrate AngularNodeAppEngine and update request handling
    - Added AngularNodeAppEngine for enhanced server-side rendering capabilities.
    - Introduced browserDistFolder for serving static files.
    - Commented out example Express API endpoints and static file serving for future implementation.
    - Updated the request handler to utilize netlifyAppEngineHandler.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,70 @@
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
+import { AngularNodeAppEngine } from '@angular/ssr/node';
 import { getContext } from '@netlify/angular-runtime/context.mjs';
+import { join } from 'path';
 
 const app = new AngularAppEngine();
 export async function netlifyAppEngineHandler(request: Request) {
   const context = getContext();
   return (await app.handle(request, context)) ?? new Response('Not found', { status: 404 });
 }
+export const browserDistFolder = join(import.meta.dirname, '../browser');
 
+// const app = express();
+export const angularApp = new AngularNodeAppEngine();
+
+/**
+ * Example Express Rest API endpoints can be defined here.
+ * Uncomment and define endpoints as necessary.
+ *
+ * Example:
+ * ```ts
+ * app.get('/api/{*splat}', (req, res) => {
+ *   // Handle API request
+ * });
+ * ```
+ */
+
+/**
+ * Serve static files from /browser
+//  */
+// app.use(
+//   express.static(browserDistFolder, {
+//     maxAge: '1y',
+//     index: false,
+//     redirect: false,
+//   }),
+// );
+
+// /**
+//  * Handle all other requests by rendering the Angular application.
+//  */
+// app.use((req, res, next) => {
+//   angularApp
+//     .handle(req)
+//     .then((response) =>
+//       response ? writeResponseToNodeResponse(response, res) : next(),
+//     )
+//     .catch(next);
+// });
+
+// /**
+//  * Start the server if this module is the main entry point, or it is ran via PM2.
+//  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+//  */
+// if (isMainModule(import.meta.url) || process.env['pm_id']) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, (error) => {
+//     if (error) {
+//       throw error;
+//     }
+
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
+
+/**
+ * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+ */
+// export const reqHandler = createNodeRequestHandler(app);
 export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
