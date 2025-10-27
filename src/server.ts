5586cd39067a297cefd40be81ea1cfba3173225f
Paulo Galdo <galdopaulo@gmail.com>
2025-10-27 00:39:09 -0300

    feat: Refactor server.ts to integrate Netlify app engine and remove unused Express code

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
+  AngularNodeAppEngine
 } from '@angular/ssr/node';
-import express from 'express';
+import { getContext } from '@netlify/angular-runtime/context.mjs';
 import { join } from 'node:path';
+
+const app = new AngularAppEngine();
+export async function netlifyAppEngineHandler(request: Request) {
+  const context = getContext();
+  return (await app.handle(request, context)) ?? new Response('Not found', { status: 404 });
+}
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
-const app = express();
+// const app = express();
 const angularApp = new AngularNodeAppEngine();
 
 /**
@@ -26,43 +30,44 @@ const angularApp = new AngularNodeAppEngine();
 
 /**
  * Serve static files from /browser
- */
-app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
-);
+//  */
+// app.use(
+//   express.static(browserDistFolder, {
+//     maxAge: '1y',
+//     index: false,
+//     redirect: false,
+//   }),
+// );
 
-/**
- * Handle all other requests by rendering the Angular application.
- */
-app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
+// /**
+//  * Handle all other requests by rendering the Angular application.
+//  */
+// app.use((req, res, next) => {
+//   angularApp
+//     .handle(req)
+//     .then((response) =>
+//       response ? writeResponseToNodeResponse(response, res) : next(),
+//     )
+//     .catch(next);
+// });
 
-/**
- * Start the server if this module is the main entry point, or it is ran via PM2.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+// /**
+//  * Start the server if this module is the main entry point, or it is ran via PM2.
+//  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+//  */
+// if (isMainModule(import.meta.url) || process.env['pm_id']) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, (error) => {
+//     if (error) {
+//       throw error;
+//     }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
-export const reqHandler = createNodeRequestHandler(app);
+// export const reqHandler = createNodeRequestHandler(app);
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
