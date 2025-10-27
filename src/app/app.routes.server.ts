a03e7b2057b5f1a0ff572b3e5662e004ee7aec04
Paulo Galdo <galdopaulo@gmail.com>
2025-10-27 00:43:27 -0300

    feat: Update server routes for cocktail list and detail views

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'cocktail/list',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'cocktail/detail/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
